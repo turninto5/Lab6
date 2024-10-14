95e42f9ff603d97793b3033648740716c4322ff7
Trace <qincu578@student.liu.se>
2024-10-14 17:01:44 +0200

    .Rproj.user always modified automactically......

--- R/DynamicProgramming.R
+++ R/DynamicProgramming.R
@@ -5,6 +5,7 @@
 #' @param
 #' x Parameter x is input, should be in type of data frame, which contains 2 vector weights and values of items.
 #' W Parameter W is the capacity of the bag, should be numeric.
+#' fast Parameter fast is in type of boolean and aims to improve performance via C++.
 #' @return This function returns a list, which contains elements which are selected and the maximum value the bag contain under its capacity.
 #' @examples
 #' v <-c(2,2,6,5,4)
@@ -15,13 +16,44 @@
 #' @name dynamicProgramming
 
 source("./R/RandomData.R")
+library(Rccp)
 
-dynamicProgramming <- function(x = NA, W = NA){
+dynamicProgramming <- function(x = NA, W = NA, fast = NA){
+    stopifnot(is.logical(fast))
     stopifnot(is.data.frame(x))
     stopifnot(is.numeric(W))
     weight <- x$w
     value <- x$v
     ssq <- matrix(data = 0, nrow = length(weight) + 1, ncol = W + 1) # solution of sub-question
+
+    if(fast){
+        cppFunction('void knapsack(std::vector<double> weight, std::vector<double> value, int w){
+          int n = weight.size();
+          int m = value.size();
+          double ssp[n][w];
+          for (int i = 0; i <= n; i++){
+            for (int j = 0; j < m; j++){
+              ssp[i][j] = 0;
+            }
+          }
+          # initialize ssp
+          
+          for(int i = 1; i <= n; i++){
+            for(int j = w; j > 0; j--){
+              ssq[i][j] = max(ssq[i-1][j], ssq[i-1][j-weight[i]] + values[i])
+            }
+          }
+          vector<int> outcome;
+          for(int i = 0; i <= n; i++){
+            if(ssq[i][w+1] > ssq[i-1][w+1]){
+              outcome.push_back(i);
+              w -= weight[i];
+            }
+          }
+        }')
+    }
+
+
 
     for (i in 1:length(weight)){
         for (j in seq(from = W + 1, to = weight[i])){
