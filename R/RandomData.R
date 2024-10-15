af626e6653060c3155261c9f3fdb06ae49f3117d
Trace <qincu578@student.liu.se>
2024-10-15 21:49:05 +0200

    documentation, but not finished

--- R/RandomData.R
+++ R/RandomData.R
@@ -1,14 +1,21 @@
 #' RandomData
 #' @description This file aims to generate input data for knapsack
 #' @name RandomData
-#' @export
+#' @examples
+#' knapsack_objects <- randomData()
+#'
+#' @export randomData
 
-RNGversion(min(as.character(getRversion()),"3.5.3"))
+randomData <- function(){
+  RNGversion(min(as.character(getRversion()),"3.5.3"))
 
-##old sampler used for backward compatibility
-## suppressWarnings() can be used so that the above warning is not displayed
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <- data.frame(w=sample(1:3500, size = n, replace = TRUE),
-                               v=runif(n = n, 0, 10000))
+  ##old sampler used for backward compatibility
+  ## suppressWarnings() can be used so that the above warning is not displayed
+  set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+  n <- 2000
+  knapsack_objects <- data.frame(w=sample(1:3500, size = n, replace = TRUE),
+                                 v=runif(n = n, 0, 10000))
+  return(knapsack_objects)
+}
+
 
