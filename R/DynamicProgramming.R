43d83d15c39d476baba06250a96f1f46c7e20ea5
Trace <qincu578@student.liu.se>
2024-10-17 17:13:31 +0200

    c++ optimization, makes it much faster for this time, and some vigenette

--- R/DynamicProgramming.R
+++ R/DynamicProgramming.R
@@ -16,41 +16,46 @@
 #' @import Rcpp
 
 
-
 dynamicProgramming <- function(x = NA, W = NA, fast = NA){
     stopifnot(is.logical(fast))
     stopifnot(is.data.frame(x))
     stopifnot("v" %in% colnames(x))
     stopifnot("w" %in% colnames(x))
     stopifnot(is.numeric(W))
-    weight <- x$w
-    value <- x$v
-    ssq <- matrix(data = 0, nrow = length(weight) + 1, ncol = W + 1) # solution of sub-question
+    weights <- x$w
+    values <- x$v
+    ssq <- matrix(data = 0, nrow = length(weights) + 1, ncol = W + 1) # solution of sub-question
 
-    for (i in 1:length(weight)){
-      for (j in seq(from = W + 1, to = weight[i] + 1)){
-        # we used profvis to analyse the lowest efficient part, and found this part can be replaced by C++
-        if(fast){
-          ssq[i + 1, j] <- updateStatus(ssq[i,j], ssq[i, j - weight[i]], value[i])
-        }
-        else{
-          ssq[i + 1, j] <- max(ssq[i, j], ssq[i, j - weight[i]] + value[i])
-        }
 
+    if (!fast){
+      for (i in 1:length(weights)){
+        value <- values[i]
+        weight <- weights[i]
+        temp <- ssq[i+1, ]
+        temp_front <- ssq[i, ]
+
+        if (W > weight){
+          for (j in seq(from = W + 1, to = weight + 1)){
+            temp[j] <- max(temp_front[j], temp_front[j - weight] + value)
+          }
+          ssq[i+1, ] <- temp
+        }
       }
     }
+    else{
+      updateStatus(ssq, weights, values, W)
+    }
+
     elements <- vector()
     for (i in seq(from = nrow(ssq), to = 2)){
       if (ssq[i, W+1] > ssq[i-1, W+1]){
         elements <- append(elements, i-1)
-        W <- W - weight[i-1]
+        W <- W - weights[i-1]
       }
     }
     outcome <- list(value = max(ssq), elements = elements)
     return (outcome)
 }
-
-
 
 
 #' updateStatus
@@ -60,12 +65,21 @@ dynamicProgramming <- function(x = NA, W = NA, fast = NA){
 #' @param load A numeric variable.
 #' @param value A numeric variable.
 #' @return A numeric element.
-#' @export
 
-updateStatus <- cppFunction({
-  'int updateStatus(double not_load, double load, double value){
-                return std::max(not_load, load + value);
-            }'
-})
+
+
+updateStatus <- cppFunction(
+  'void updateStatus(Rcpp::NumericMatrix& ssq, Rcpp::NumericVector weights, Rcpp::NumericVector values, int w){
+      int value;
+      int weight;
+      for(int i = 0; i < ssq.nrow() - 1; i++){
+        value = values[i];
+        weight = weights[i];
+        for (int j = w; j >= weight; j--){
+          ssq(i+1,j) = std::max(ssq(i,j),  ssq(i, j - weight) + value);
+        }
+      }
+  }'
+)
 
 
